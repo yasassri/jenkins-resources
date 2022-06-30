52681384210c904c4bd83cbca4e79f0f314467be
Yasassri Ratnayake <ycrnet@gmail.com>
2022-06-30 08:37:55 -0400

    Update BringtemporarilyDisabledAgentsOnline.groovy

--- BringtemporarilyDisabledAgentsOnline.groovy
+++ BringtemporarilyDisabledAgentsOnline.groovy
@@ -1,6 +1,20 @@
+// cliOnline() seems to be depricated
 def jenkinsNodes = Jenkins.instance.getNodes()
   for(def node: jenkinsNodes) {
       if (node.getComputer().isOffline()){  
           node.getComputer().cliOnline()
+      }
+  }
+
+
+// A non depricated approach. Build cause is only needed when setting to offline
+import hudson.slaves.OfflineCause.UserCause
+
+def jenkinsNodes = Jenkins.instance.getNodes()
+  for(def node: jenkinsNodes) {
+      if (node.getComputer().isTemporarilyOffline()){  
+           println(node.getComputer().class)
+           UserCause cause =  new UserCause(User.current(), "This is a automated process!!")
+           node.getComputer().setTemporarilyOffline(false, cause)
       }
   }
